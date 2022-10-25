56126ed7c7e57e0bf79717083a62a8093b416345
Li <li@gmail.com>
2022-10-25 03:23:14 +0300

    feat: add move utem and win function

--- js/index.js
+++ js/index.js
@@ -1,15 +1,30 @@
-console.log("hello")
-let norm = 4;
-let container = document.createElement('div');
-let itog = Array.from(Array(norm ** 2).keys());
+
+let norm = 3;
+let count = 0;
 
 
+//create arr
+function create_arr(n) {
+    count = 0;
+    return Array.from(Array(n ** 2).keys());
+}
+//mix arr
 function shuffle(arr) {
     return arr.sort(() => Math.random() - 0.5)
 }
-let random_arr = nrr(shuffle(itog));
-// console.log(random_arr);
+// del na mini arr
+function nrr(arr) {
+    let y = [];
+    let s = Math.sqrt(arr.length);
+    for (let i = 0; i < arr.length; i += s) {
+        y.push(arr.slice(i, i + s));
+    }
+    return y;
+}
 
+
+
+//write arr in html
 function bloki(x) {
     let z = "";
     let d = "<div class='item-block'>";
@@ -23,38 +38,75 @@ function bloki(x) {
             }
         }
     }
+
     return z;
-    //
-
 }
 
+//size \
+let sizes = document.querySelectorAll('.item-size');
+console.log(sizes);
 
-container.className = "game-block";
-switch (norm) {
-    case 3: container.classList.add('grid-three'); break;
-    case 4: container.classList.add('grid-four'); break;
-    case 5: container.classList.add('grid-five'); break;
-    case 6: container.classList.add('grid-six'); break;
-    case 7: container.classList.add('grid-seven'); break;
-    case 8: container.classList.add('grid-eight'); break;
+
+for (let i = 0; i < sizes.length; i++) {
+    sizes[i].addEventListener('click', function () {
+        // console.log(sizes[i].classList[1]);
+        norm = sizes[i].classList[1];
+        console.log(norm);
+    })
 }
-container.innerHTML = bloki(random_arr);
-// console.log(random_arr);
-document.body.append(container);
 
-function nrr(arr) {
-    let y = [];
-    let s = Math.sqrt(arr.length);
-    for (let i = 0; i < arr.length; i += s) {
-        y.push(arr.slice(i, i + s));
+///////////////////////////START!!!!
+let starts = document.querySelector('.start');
+
+//itog massiv
+let itog, random_arr, itogger;
+function addcl(n) {
+    if (n == 3) {
+        return 'grid-three';
     }
-    return y;
+    if (n == 4) {
+        return 'grid-four';
+    }
+    if (n == 5) {
+        return 'grid-five';
+    }
+    if (n == 6) {
+        return 'grid-six';
+    }
+    if (n == 7) {
+        return 'grid-seven';
+    }
+    if (n == 8) {
+        return 'grid-eight';
+    }
 }
-// console.log(nrr(random_arr));
-// let cont = document.getElementsByClassName('item-block');
 
+let container = document.createElement('div');
+let counter = document.createElement('div');
+console.log(norm + " hi switch norm");
+
+starts.addEventListener('click', function (e) {
+    count = 0;
+    container.className = "game-block";
+    container.classList.add(addcl(norm));
+    itog = create_arr(norm);
+    //itoger
+    itog.splice(0, 1);
+    itog.push(0);
+    itogger = nrr(itog);
+
+    random_arr = nrr(shuffle(itog));
+    container.innerHTML = bloki(random_arr);
+    counter.innerHTML = 0;
+    document.body.append(container);
+    document.body.append(counter);
+})
+
+//MOVE items
 
 function moveItem(n, ar) {
+    z.innerHTML = "";
+    console.log(z.innerHTML);
     let razm = ar.length;
     let coord_zer = [];
     let coord_ch = []
@@ -75,14 +127,25 @@ function moveItem(n, ar) {
     if (+check(coord_ch, coord_zer) < 5) {
         ar[coord_ch[0]][coord_ch[1]] = 0;
         ar[coord_zer[0]][coord_zer[1]] = +n;
+        count++;
     }
 
     container.innerHTML = bloki(ar);
+    counter.innerHTML = count;
+    document.body.append(counter);
     document.body.append(container);
+
+    //win
+    if (ar.join("") == itogger.join("")) {
+        let z = document.createElement('div')
+        z.innerHTML = "YOU WIN!!!!!!";
+        z.className = "win";
+        document.body.append(z);
+        container.classList.add('block');
+        document.body.append(container);
+    }
     return ar;
 }
-
-
 
 function check(a, z) {
     if (a[0] == z[0]) {
@@ -113,9 +176,24 @@ window.addEventListener('click', function (e) {
         // console.log(random_arr.indexOf(+ch));
         // console.log(moveItem(ch, random_arr));
         random_arr = moveItem(ch, random_arr);
-
-
     }
-    //console.log(e.path[0].className);
-
 })
+
+
+//time
+let timer = document.querySelector('.time');
+
+
+
+function Timer() {
+    let elem = document.getElementById('timer');
+    elem.value = +elem.value + 1;
+}
+function start() {
+    window.TimerId = window.setInterval(Timer, 1000);
+}
+function stop() {
+    window.clearInterval(window.TimerId);
+}
+
+
